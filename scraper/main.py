5d8eb3d7b4144940cdfd0eada975ae7a3716cf23
Adir Nisim <adirnnisim1@gmail.com>
2022-06-23 01:38:26 +0300

    Added jupyter notebook

--- scraper/main.py
+++ scraper/main.py
@@ -2,23 +2,21 @@ import os
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
-import pyodbc
 import pandas as pd
 
 
 # Global variables
-autoScroll = 0  # Flag for autoscroll in NadlanGov.
+autoScroll = 1  # Flag for autoscroll in NadlanGov.
 waitTime = 10  # Waiting time for loading the search value transactions.
 slow_wait_time = 5
 scrollWaitingTime = 10
-topHeight = 55*100000 # Maximum value for loading 50K values in a table of NadlanGov 55 inches per line.
+topHeight = 55*1000000
+# Maximum value for loading 50K values in a table of NadlanGov 55 inches per line.
 city = "ראשון לציון"
 en_city = "Rishon_le-zion"
-pool_size = 3
-scroll_down_limit = 1
+pool_size = 4
+scroll_down_limit = 100
 directory_name = os.path.dirname(__file__)
 
 # For multithreading CSV files
@@ -36,15 +34,12 @@ def get_topics(web_driver: webdriver, headers: dict):
     header_elements = web_driver.find_element(By.XPATH, "//*[contains(@class, 'myTable')]/div[2]/div[5]")
     # Columns titles each of Row as a Key for dictionary.
     for header in header_elements.find_elements(by=By.TAG_NAME, value='button'):
-        if header != '':
+        if header.get_attribute('aria-label') != '':
             headers[header.get_attribute('aria-label')] = []
-    # Addition 2 topics lists
-    headers["תאריכי עבר"] = []
-    headers["מחירי עבר"] = []
 
 
 # Scrolling down the table
-def scroll_down_table(web_driver: webdriver,auto: int):
+def scroll_down_table(web_driver: webdriver, auto: int):
     scroll_number = 0
     # Scroll down for loading more data in table
     if auto:
@@ -52,11 +47,12 @@ def scroll_down_table(web_driver: webdriver,auto: int):
         while auto:
             scroll_number += 1
             web_driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(scrollWaitingTime)
+            loader = web_driver.find_element(By.CLASS_NAME, "loading")
+            while not loader.get_attribute('class').__contains__('ng-hide'):
+                continue
             height = web_driver.execute_script("return document.body.scrollHeight;")
-            time.sleep(scrollWaitingTime)
-            print("height", height)
-            print("\n Check_height", check_height)
+            print("scroll_down_limit", scroll_down_limit)
+            print("scroll_number", scroll_number)
             if height == check_height or height > topHeight or scroll_number > scroll_down_limit:
                 auto = 0
                 break
@@ -71,13 +67,14 @@ def get_data_from_table(web_driver: webdriver, headers: dict):
     my_table = web_driver.find_element(By.XPATH, "//*[contains(@class, 'tableBody')]")
     # rows data, each value of data by topic will be insert for the belong list in the dictionary.
     for row in my_table.find_elements(by=By.CLASS_NAME, value='rbutton'):
-        time.sleep(5)
+        web_driver.execute_script(f"arguments[0].scrollIntoView(true);", row)
+        web_driver.execute_script(f"window.scrollBy(0, -500);")
         for i, field in enumerate(row.find_elements(by=By.CLASS_NAME, value='tableCol')):
             elements = field.find_elements(by=By.TAG_NAME, value='div')
             if field.tag_name == 'div' and len(elements) > 0:
                 value = elements[0].get_attribute('title')
                 if value == '':
-                    value = 'NaN' # if value is empty NaN will be insert.
+                    value = 'NaN'  # if value is empty NaN will be insert.
                 if i > 8:
                     break
                 headers[list(headers.keys())[i]].append(value)
@@ -85,45 +82,36 @@ def get_data_from_table(web_driver: webdriver, headers: dict):
                 headers[list(headers.keys())[i]].append('NaN')
         # Getting the Addition Data of history
         # Clicking the line
-        row.click()
-        time.sleep(slow_wait_time)
+        row.find_elements(By.CLASS_NAME, 'tableCol')[0].find_element(By.TAG_NAME, 'div').click()
         # Getting the Elements
-        try:
-            # Getting Data from inside button of history dates and prices
-            history_data = my_table.find_element(by=By.CLASS_NAME, value="resultOptions")
-            history_price = history_data.find_elements(by=By.CLASS_NAME, value="historyPrice")
-            history_date = history_data.find_elements(by=By.CLASS_NAME, value="historyDate")
-            # Adding Values into lists
-            for field_of_dates in history_date:
-                    dates_of_history.append(field_of_dates.text)
-            for field_of_price in history_price:
-                    prices_of_history_dates.append(field_of_price.text)
-            if dates_of_history:
-                headers["תאריכי עבר"].append(dates_of_history[:])
-            else:
-                # If null
-                headers["תאריכי עבר"].append("NaN")
-            if prices_of_history_dates:
-                headers["מחירי עבר"].append(prices_of_history_dates[:])
-            else:
-                # if null
-                headers["מחירי עבר"].append("NaN")
-        except:
-            headers["תאריכי עבר"].append("NaN")
-            headers["מחירי עבר"].append("NaN")
+        time.sleep(1)
+        # Getting Data from inside button of history dates and prices
+        history_data = my_table.find_element(by=By.CLASS_NAME, value="resultOptions")
+        history_prices = history_data.find_elements(by=By.CLASS_NAME, value="historyPrice")
+        history_dates = history_data.find_elements(by=By.CLASS_NAME, value="historyDate")
+        history_records = []
+        # Adding Values into lists
+        for field_of_dates in history_dates:
+            record = {'history_date': field_of_dates.text}
+            history_records.append(record)
+        for i, field_of_price in enumerate(history_prices):
+            history_records[i]['history_price'] = field_of_price.text
+        if history_records:
+            for history_record in history_records:
+                for column in headers:
+                    headers[column].append(headers[column][-1])
+                headers['סכום'][-1] = history_record['history_price']
+                headers['יום מכירה'][-1] = history_record['history_date']
 
         # clearing list for next time
         dates_of_history.clear()
         prices_of_history_dates.clear()
 
-        # time pause
-        time.sleep(waitTime)
-
         # click button again
-        row.click()
+        row.find_elements(By.CLASS_NAME, 'tableCol')[0].find_element(By.TAG_NAME, 'div').click()
 
 
-bedroom_filtering_range = range(3, pool_size + 1)
+bedroom_filtering_range = range(pool_size, pool_size + 1)
 for bedroom_filter in bedroom_filtering_range:
     # Open webdriver for multi threading
     # Web crawling start.
@@ -144,13 +132,17 @@ for bedroom_filter in bedroom_filtering_range:
     search_button = driver.find_element(By.ID, "submitSearchBtn")
     search_button.click()
 
+    time.sleep(1)
+
     # Get new search link
-    time.sleep(waitTime)
+    loader = driver.find_element(By.CLASS_NAME, "loading")
+    while not loader.get_attribute('class').__contains__('ng-hide'):
+        continue
 
     # Dictionary { Key: topic, Value: List }
     topics = {}
     # Get new search link
-    time.sleep(waitTime)
+    time.sleep(1)
     # Getting keys for dic : topics
     get_topics(driver, topics)
 
@@ -162,8 +154,12 @@ for bedroom_filter in bedroom_filtering_range:
         div_button.find_element(By.CLASS_NAME, "roomsFillter").find_elements(By.TAG_NAME, "button")[bedroom_filter]
     selection.click()
 
+    time.sleep(1)
+
     # Time sleep
-    time.sleep(waitTime)
+    loader = driver.find_element(By.CLASS_NAME, "loading")
+    while not loader.get_attribute('class').__contains__('ng-hide'):
+        continue
 
     # scrolling down the table
     scroll_down_table(driver, autoScroll)
@@ -173,13 +169,10 @@ for bedroom_filter in bedroom_filtering_range:
     time.sleep(waitTime)
     # Getting data from table
     get_data_from_table(driver, topics)
-    # remove empty topic
-    del topics['']
 
     # create data frame
     df = pd.DataFrame(data=topics)
     # create CSV File
-    df.to_csv("fornow2.csv", encoding='utf-8-sig')
+    # df.to_csv("fornow2.csv", encoding='utf-8-sig')
+    os.mkdir(os.path.realpath(f"../CSV files/{en_city}"))
     df.to_csv(os.path.realpath(f"../CSV files/{en_city}/filter_by_{bedroom_filter}.csv"),encoding='utf-8-sig')
-
-
